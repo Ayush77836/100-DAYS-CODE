5547d296534626a359f4864ba7c8649257d93a87
Ayush77836 <singhsanjaykumar3866@gmail.com>
2025-08-24 23:51:34 +0530

    Add files via upload

---
+++ exp3.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+ int main(){
+   float radius,area,circumference;
+
+   printf(" enter radius:");
+
+    scanf("%f",&radius);
+
+    area=3.14*radius*radius;
+
+    circumference=2*3.14*radius;
+
+    printf("area is %f", area);
+
+    printf("circumference is %f", circumference);
+
+   
+    
+
+    return 0;
+}
