c085186d8310a2d32930635a1356dd4c7384d923
Ayush77836 <singhsanjaykumar3866@gmail.com>
2025-09-01 19:11:21 +0530

    Add files via upload

---
+++ exp22.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+    int main() {
+        int units;
+        float total_bill = 0;
+        printf("Enter electricity unit consumed");
+
+        scanf("%d", &units);
+        if (units <= 50) {
+        bill = units * 0.50;
+    } 
+    else if (units <= 150) {
+        bill = (50 * 0.50) + (units - 50) * 0.75;
+    } 
+    else if (units <= 250) {
+        bill = (50 * 0.50) + (100 * 0.75) + (units - 150) * 1.20;
+    } 
+    else {
+        bill = (50 * 0.50) + (100 * 0.75) + (100 * 1.20) + (units - 250) * 1.50;
+    }
+
+
+    total = bill + (bill * 0.20);
+
+    printf("Electricity Bill = Rs. %.2f\n", total);
+
+       
+        return 0;
+    }
